af791054893bd756af07c2c8fc03703a47560f88
AHSAN <ahsan1617k@gmail.com>
2024-11-16 10:56:15 +0500

    Time: 7 ms (96.10%) | Memory: 37.3 MB (23.32%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> ans;
-        map<int,int> mp1, mp2;
-        int n = nums1.size();
-        int m = nums2.size();
-        for(int i=0;i<n;i++){
-            mp1[nums1[i]]++;
-        }
-        for(int i=0;i<m;i++){
-            mp2[nums2[i]]++;
-        }
-        vector<int> ans1;
-        for(int i=0;i<n;i++){
-            if(mp2.find(nums1[i]) == mp2.end() && mp1[nums1[i]] != 0){
-                ans1.push_back(nums1[i]);
-                mp1[nums1[i]] = 0;
+        unordered_set<int>set1(nums1.begin(),nums1.end());
+        unordered_set<int>set2(nums2.begin(),nums2.end());
+        vector<int>ans1,ans2;
+        for(int n: set1){
+            if(set2.find(n)==set2.end()){
+                ans1.push_back(n);
             }
         }
-        ans.push_back(ans1);
-        vector<int> ans2;
-        for(int i=0;i<m;i++){
-            if(mp1.find(nums2[i]) == mp1.end() && mp2[nums2[i]] != 0){
-                ans2.push_back(nums2[i]);
-                mp2[nums2[i]] = 0;
+        for(int n: set2){
+            if(set1.find(n)==set1.end()){
+                ans2.push_back(n);
             }
         }
-        ans.push_back(ans2);
-        return (ans);
+        return {ans1,ans2};
     }
 };
