30cd3a652e3cb1c0cdce640828a41f571811e51d
AHSAN <ahsan1617k@gmail.com>
2024-12-18 08:10:31 +0500

    Time: 3 ms (41.42%) | Memory: 40.3 MB (41.10%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
         vector<int>res(n,1);
-        int pre=1;
-        for(int i=0;i<n;++i){
-            res[i]=pre;
-            pre*=nums[i];
-        }        
-        int post=1;
+        int prefix=1;
+        for(int i=0;i<n;i++){
+            res[i]*=prefix;
+            prefix*=nums[i];
+        }
+        int suffix=1;
         for(int i=n-1;i>=0;--i){
-            res[i]*=post;
-            post*=nums[i];
+            res[i]*=suffix;
+            suffix*=nums[i];
         }
         return res;
     }
