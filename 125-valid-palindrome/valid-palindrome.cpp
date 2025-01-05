024219ea3e00ec5d7a1d47a5afffe7b9d98a86d0
AHSAN <ahsan1617k@gmail.com>
2025-01-05 09:16:20 +0500

    Time: 4 ms (16.47%) | Memory: 10 MB (27.05%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int st=0;
-       int en=s.size()-1;
-       while(st<=en){
-        if(!isalnum(s[st])){
+        int st=0;
+        int end=s.size()-1;
+        while(st<=end){
+            if(!isalnum(s[st])){
+                st++;
+                continue;
+            }
+            if(!isalnum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[st])!=tolower(s[end])){
+                return false;
+            }
             st++;
-            continue;
+            end--;
         }
-        if(!isalnum(s[en])){
-            en--;
-            continue;
-        }
-        if(tolower(s[st])!=tolower(s[en])){
-            return false;
-        }else{
-            st++;
-            en--;
-        }
-       }
-       return true;
+        return true;
     }
 };
